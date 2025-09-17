e1044a2a987df14fb48aee963fbc2612d5909191
Azul Cerulean <azul.sama@outlook.com>
2025-09-17 12:04:19 -0400

    update sets.py
    more notes

--- Sets/intro.py
+++ Sets/intro.py
@@ -13,9 +13,42 @@ workshop = {
 # Complete the key-value pairs below
 
 python_set = {
-    'definition': '',
-    'properties': '',
-    'membership': '',
+    'definition': 'An unordered collection of unique elements.',
+    'properties': 'No order & no duplicate values.',
+    'membership': 'An element of a set is called a member.',
 }
 
 # You may code-along with my video below!
+
+# my_first_set = {'Hello Sets!'}
+# print(my_first_set) # {'Hello Sets!'}
+# print(type(my_first_set)) # <class 'set'>
+# print(len(my_first_set)) # 1
+
+# Sets - order does not matter
+# print({1, 2, 3} == {3, 2, 1}) # True
+
+# Lists - order DOES matter
+# print([1, 2, 3] == [3, 2, 1]) # False
+
+# print({1, 2, 3, 4, 1, 2, 3, 4,1, 2}) #{1, 2, 3, 4}
+# print(len({1, 2, 3, 4, 1, 2, 3, 4,1, 2})) # 4
+
+# fours = {4, 4.0}
+# print(fours) # {4}
+# print(4 == 4.0) # True
+# print({4.0, 4}) # 4.0
+# they are both 4 and it will display the first one, left to right
+# challenge = {4.0, 4, 5.0, 5}
+# print(challenge) # {4.0, 5.0}
+
+# twos = {'two', 2, 2.22, ('dos', 'Spanish')}
+# print(twos) # {2, 2.22, ('dos', 'Spanish'), 'two'}
+# booleans = {True, False}
+# print(booleans) # {False, True}
+# none_set = {None}
+# print(none_set) # {None}
+# print({[1,2,3]}) # TypeError: unhashable type: 'list'
+# print({{'hello': 'world!'}}) # TypeError: unhashable type: 'dict'
+# print({{'hello sets!'}}) # TypeError: unhashable type: 'set'
+
