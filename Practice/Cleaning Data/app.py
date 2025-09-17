ccfbe4b911a3a6f170e56aa970c536b59c673290
Azul Cerulean <azul.sama@outlook.com>
2025-09-17 19:06:00 -0400

    completed cleaning data
    completed cleaning data

--- Practice/Cleaning Data/app.py
+++ Practice/Cleaning Data/app.py
@@ -8,3 +8,22 @@ from data import data
 # 4) Keep the rest of the info the same
 # 5) Complete this in a function(s)
 # 6) Save the data into a new data structure: a list of dictionaries
+
+
+def clean_data(data):
+    cleaned = []
+    for user in data:
+        fixed = {}
+        fixed["email"] = user["email"]
+        fixed["first_name"] = user["name"].split(" ")[0]
+        fixed["last_name"] = user["name"].split(" ")[-1]
+        fixed["date_joined"] = user["date_joined"]
+        if user["admin"] == "True":
+            fixed["admin"] = True
+        else:
+            fixed["admin"] = False
+        fixed["id"] = int(user["id"])
+        cleaned.append(fixed)
+    return cleaned
+
+print(clean_data(data))
