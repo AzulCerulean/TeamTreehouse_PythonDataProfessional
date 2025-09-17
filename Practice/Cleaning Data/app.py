066c5d69e0c95ebea87d542ceef5305608d3424b
Azul Cerulean <azul.sama@outlook.com>
2025-09-17 18:32:46 -0400

    added practice cleaning data
    added practice cleaning data

---
+++ Practice/Cleaning Data/app.py
@@ -0,0 +1,10 @@
+from data import data
+
+
+# INSTRUCTIONS
+# 1) Split the full name into two fields, first name and last name
+# 2) Convert the admin field to a boolean value
+# 3) Convert the id to an integer
+# 4) Keep the rest of the info the same
+# 5) Complete this in a function(s)
+# 6) Save the data into a new data structure: a list of dictionaries
