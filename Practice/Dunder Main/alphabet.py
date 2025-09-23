e765898c4743932692f740272ace41a12e7f16b3
Azul Cerulean <azul.sama@outlook.com>
2025-09-23 16:58:09 -0400

    added a comment

--- Practice/Dunder Main/alphabet.py
+++ Practice/Dunder Main/alphabet.py
@@ -10,6 +10,7 @@ def sort_list(list):
     return print(sorted(list)[0])
 # can also use return min(list) to return the smallest character (num and/or letter)
 
+# can place testing withing the dunder main to hide it when the main file runs
 if __name__ == "__main__":
     word_list = ["hamster", "turtle", "cat", "bird"]
     print(sorted(word_list)[0])
