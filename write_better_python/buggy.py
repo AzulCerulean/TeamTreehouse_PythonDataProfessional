2f7e75991d14aae95a48821b6f8b811ac2ee599d
Azul Cerulean <azul.sama@outlook.com>
2025-09-23 19:04:08 -0400

    pdb tracing to debug

--- write_better_python/buggy.py
+++ write_better_python/buggy.py
@@ -1,1 +1,12 @@
+# import pdb
+
 my_list = [5, 2, 1, True, "abcdefg", 3, False, 4]
+
+# pdb.set_trace()
+# can also  use it in one line and the only time a semi-colon is ok
+# import pdb; pdb.set_trace()
+# you usually remove it one debugged
+del my_list[3]  # [5, 2, 1, "abcdefg", 3, False, 4]
+del my_list[3]  # [5, 2, 1, 3, False, 4]
+del my_list[4]  # [5, 2, 1, 3, False, 4]
+print(my_list)
