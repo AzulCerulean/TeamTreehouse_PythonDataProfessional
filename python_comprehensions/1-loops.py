8bdb2437bacd784d0eab8e46488387512d420c65
Azul Cerulean <azul.sama@outlook.com>
2025-09-24 14:42:17 -0400

    pep-8 spacing

--- python_comprehensions/1-loops.py
+++ python_comprehensions/1-loops.py
@@ -2,18 +2,18 @@
 old_nametags = ['laura', 'dustin', 'rachel']
 new_nametags = []
 for nametag in old_nametags:
-  new_nametags.append(nametag.capitalize())
-  
+    new_nametags.append(nametag.capitalize())
+
 ########
 
 # for loop - make the 'welcome' string all caps
 old_banner = "welcome"
 new_banner = ""
 for letter in old_banner:
-  new_banner += letter.upper()
+    new_banner += letter.upper()
 
 ########
-  
+
 # print statements
 print(new_nametags)
 print(new_banner)
