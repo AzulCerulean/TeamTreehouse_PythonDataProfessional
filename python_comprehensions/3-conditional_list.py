45d1695aaaf92640825758691c5b8eda06b451c8
Azul Cerulean <azul.sama@outlook.com>
2025-09-24 15:12:22 -0400

    updated 3-conditional_list.py and 4-conditional_expression.py
    completed exercises

--- python_comprehensions/3-conditional_list.py
+++ python_comprehensions/3-conditional_list.py
@@ -1,25 +1,29 @@
 # for loop - Return a list with only the names with 6+ letters
-old_nametags = ['laura', 'dustin', 'rachel']
-new_nametags = []
-for nametag in old_nametags:
-  if len(nametag)>=6:
-    new_nametags.append(nametag)
+# old_nametags = ['laura', 'dustin', 'rachel']
+# new_nametags = []
+# for nametag in old_nametags:
+#     if len(nametag) >= 6:
+#         new_nametags.append(nametag)
 
 # conditional list syntax template
-#new_nametags = [expression for temporary_variable in original_iterable if condition]
+new_nametags = [nametag for nametag in ['laura', 'dustin', 'rachel']
+                if len(nametag) >= 6]
 
 
 # for loop - return a string with only the vowels
 old_banner = "welcome"
 new_banner = ""
 for letter in old_banner:
-  if letter in 'aeiou':
-    new_banner += letter
+    if letter in 'aeiou':
+        new_banner += letter
 
 # conditional list syntax template
-#new_banner = [expression for temporary_variable in original_iterable if condition]
+# new_banner = [expression for temporary_variable in original_iterable
+#               if condition]
+new_banner = "".join([letter for letter in "welcome"
+                      if letter in 'aeiou'])
 
 
 # Print statements
-print(new_nametags)
-print(new_banner)
+print(new_nametags)  # ['dustin', 'rachel']
+print(new_banner)  # eoe
--- python_comprehensions/4-conditional_expression.py
+++ python_comprehensions/4-conditional_expression.py
@@ -1,28 +1,30 @@
 # for loop - Return a list with 6+ lettered names capitialized
-old_nametags = ['laura', 'dustin', 'rachel']
-new_nametags = []
-for nametag in old_nametags:
-  if len(nametag)>=6:
-    new_nametags.append(nametag.capitalize())
-  else:
-    new_nametags.append(nametag)
+# old_nametags = ['laura', 'dustin', 'rachel']
+# new_nametags = []
+# for nametag in old_nametags:
+#     if len(nametag) >= 6:
+#         new_nametags.append(nametag.capitalize())
+#     else:
+#         new_nametags.append(nametag)
 
 # conditional expression syntax template
-#new_nametags = [expression if condition else expression for temporary_variable in original_iterable]
+new_nametags = [nametag.capitalize() if len(nametag) >= 6 else nametag
+                for nametag in ['laura', 'dustin', 'rachel']]
 
 # for loop - make only the vowels uppercase
-old_banner = "welcome"
-new_banner = ""
-for letter in old_banner:
-  if letter in 'aeiou':
-    new_banner += letter.upper()
-  else:
-    new_banner += letter
+# old_banner = "welcome"
+# new_banner = ""
+# for letter in old_banner:
+#     if letter in 'aeiou':
+#         new_banner += letter.upper()
+#     else:
+#         new_banner += letter
 
 # conditional expression syntax template
-#new_banner = [expression if condition else expression for temporary_variable in original_iterable]
+new_banner = "".join([letter.upper() if letter in 'aeiou' else letter
+                      for letter in "welcome"])
 
 
 # print statements
-print(new_nametags)
-print(new_banner)
+print(new_nametags)  # ['laura', 'Dustin', 'Rachel']
+print(new_banner)  # wElcOmE
