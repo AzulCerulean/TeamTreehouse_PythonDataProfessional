c0bec286c40c4735330e5313c8ed65930487c964
Azul Cerulean <azul.sama@outlook.com>
2025-09-24 15:34:44 -0400

    Update 6-set_comprehension.py
    completed exercise

--- python_comprehensions/6-set_comprehension.py
+++ python_comprehensions/6-set_comprehension.py
@@ -1,26 +1,27 @@
 # for loop - Create a set of unique test scores
-test_scores = [99, 53, 74, 66, 45, 74]
-unique_scores = set()
-for score in test_scores:
-  unique_scores.add(score)
+# test_scores = [99, 53, 74, 66, 45, 74]
+# unique_scores = set()
+# for score in test_scores:
+#     unique_scores.add(score)
 
 # set comprehension syntax template
-#unique_scores = {expression for temporary_variable in original_iterable}
+unique_scores = {score for score in [99, 53, 74, 66, 45, 74]}
 
 ########
 
 # for loop - Create a set of unique test scores over 70
-test_scores = [99, 53, 74, 66, 45, 74]
-unique_scores_over_70 = set()
-for score in test_scores:
-  if score > 70:
-    unique_scores_over_70.add(score)
+# test_scores = [99, 53, 74, 66, 45, 74]
+# unique_scores_over_70 = set()
+# for score in test_scores:
+#     if score > 70:
+#         unique_scores_over_70.add(score)
 
 # set comprehension syntax template
-#unique_scores_over_70 = {expression for temporary_variable in original_iterable}
+unique_scores_over_70 = {score for score in [99, 53, 74, 66, 45, 74]
+                         if score > 70}
 
 ########
 
 # print statements
-print(unique_scores)
-print(unique_scores_over_70)
+print(unique_scores)  # {66, 99, 74, 45, 53}
+print(unique_scores_over_70)  # {74, 99}
