2161c185ff80c7b3b7af124fd1444fc80ef5bad1
Azul Cerulean <azul.sama@outlook.com>
2025-09-24 14:53:40 -0400

    Update 2-list_comprehension.py

--- python_comprehensions/2-list_comprehension.py
+++ python_comprehensions/2-list_comprehension.py
@@ -1,25 +1,26 @@
 # for loop - Capitalize all names in the nametags list
-old_nametags = ['laura', 'dustin', 'rachel']
-new_nametags = []
-for nametag in old_nametags:
-    new_nametags.append(nametag.capitalize())
+# old_nametags = ['laura', 'dustin', 'rachel']
+# new_nametags = []
+# for nametag in old_nametags:
+#     new_nametags.append(nametag.capitalize())
 
 # list comprehension syntax template
-# new_nametags = [expression for temporary_variable in original_iterable]
+new_nametags = [nametag.capitalize() for nametag in
+                ['laura', 'dustin', 'rachel']]
 
 ########
 
 # for loop - make the 'welcome' string all caps
-old_banner = "welcome"
-new_banner = ""
-for letter in old_banner:
-    new_banner += letter.upper()
+# old_banner = "welcome"
+# new_banner = ""
+# for letter in old_banner:
+#     new_banner += letter.upper()
 
 # list comprehension syntax template
-# new_banner = [expression for temporary_variable in original_iterable]
+new_banner = "".join([letter.upper() for letter in "welcome"])
 
 ########
 
 # print statements
-# print(new_nametags)
-# print(new_banner)
+print(new_nametags)  # ['Laura', 'Dustin', 'Rachel']
+print(new_banner)  # WELCOME
