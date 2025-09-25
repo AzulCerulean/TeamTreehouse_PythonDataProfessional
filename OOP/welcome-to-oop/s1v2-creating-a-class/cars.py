315dbf14ca5261066cc19ce80fadbf8736f8e04c
Azul Cerulean <azul.sama@outlook.com>
2025-09-25 17:18:52 -0400

    Update cars.py

---
+++ OOP/welcome-to-oop/s1v2-creating-a-class/cars.py
@@ -0,0 +1,9 @@
+class Car:
+    pass
+
+
+my_car = Car()
+
+print(my_car)  # <__main__.Car object at 0x10f862a50>
+print(type(my_car))  # <class '__main__.Car'>
+print(isinstance(my_car, Car))  # True
