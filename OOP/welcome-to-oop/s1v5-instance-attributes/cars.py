e47086eb5f54f5a988f53ad9f8665b3c13a1e179
Azul Cerulean <azul.sama@outlook.com>
2025-10-01 13:54:24 -0400

    instance attributes complete

--- OOP/welcome-to-oop/s1v5-instance-attributes/cars.py
+++ OOP/welcome-to-oop/s1v5-instance-attributes/cars.py
@@ -1,21 +1,23 @@
 class Car:
-  # class attributes
-  wheels = 4
-  doors = 2
-  engine = True
+    # class attributes
+    wheels = 4
+    doors = 2
+    engine = True
+
+    # default attributes at the end
+    def __init__(self, model, year, make="Ford"):
+        self.make = make
+        self.model = model
+        self.year = year
 
 
-car_one = Car()
-car_two = Car()
-print(car_one.doors)
-print(car_two.doors)
-print(Car.doors)
+car_one = Car("Model T", 1908)
+car_two = Car("Phantom", 2020, "Rolls Royce")
+print(car_one.make)  # Ford
+print(car_two.make)  # Rolls Royce
+# print(Car.make). # error, .make is an instance attribute, Car is
+# not an instance
 
-car_one.doors = 6
-Car.doors = 3
-print(f'Car One: {car_one.doors}')
-print(id(car_one.doors))
-print(f'Car Two: {car_two.doors}')
-print(id(car_two.doors))
-print(f'Class Car: {Car.doors}')
-print(id(Car.doors))
+car_one.year = 2015
+print(car_one.year)  # 2015
+print(car_two.year)  # 2020
