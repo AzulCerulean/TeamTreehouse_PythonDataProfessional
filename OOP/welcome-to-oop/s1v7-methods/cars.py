7b899efb9ff03ab847bb967cbafcdb5f2dbe2564
Azul Cerulean <azul.sama@outlook.com>
2025-10-01 14:32:38 -0400

    methods complete

--- OOP/welcome-to-oop/s1v7-methods/cars.py
+++ OOP/welcome-to-oop/s1v7-methods/cars.py
@@ -5,15 +5,44 @@ class Car:
     engine = True
 
     # The Initializer
-    def __init__(self, model, year, make="ford"):
+    def __init__(self, model, year, make="Ford"):
         # Instance attributes
         self.make = make
         self.model = model
-        self.year =year
+        self.year = year
+        self.gas = 100
+        self.is_moving = False
+
+    def stop(self):
+        if self.is_moving:
+            print('The car has stopped')
+            self.is_moving = False
+        else:
+            print('The car has already stopped')
+
+    def go(self, speed):
+        if self.use_gas():
+            if not self.is_moving:
+                print('The car starts moving')
+                self.is_moving = True
+            print(f'The car is going {speed}')
+        else:
+            print('You have run out of gas')
+            self.stop()
+
+    def use_gas(self):
+        self.gas -= 50
+        if self.gas <= 0:
+            return False
+        else:
+            return True
 
 
 car_one = Car('Model T', 1908)
-car_two = Car('Phantom', 2020, 'Rolls Royce')
+car_one.stop()
+car_one.go('slow')
+car_one.go('fast')
+car_one.stop()
+car_one.stop()
 
-print(car_one.make)
-print(car_two.make)
+car_two = Car('Phantom', 2020, 'Rolls Royce')
