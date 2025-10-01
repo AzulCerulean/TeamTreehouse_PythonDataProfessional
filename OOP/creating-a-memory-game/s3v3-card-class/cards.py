5c1582bafc57b328d7707a2f88aff636ea5b3246
Azul Cerulean <azul.sama@outlook.com>
2025-10-01 16:53:37 -0400

    card class completed

--- OOP/creating-a-memory-game/s3v3-card-class/cards.py
+++ OOP/creating-a-memory-game/s3v3-card-class/cards.py
@@ -1,6 +1,27 @@
 # class Card
+class Card:
     # hold a card
-    # know whether it's been matched or not
     # hold the card's location
+    # know whether it's been matched or not
+    def __init__(self, word, location):
+        self.word = word
+        self.location = location
+        self.matched = False
+
     # know if two cards are equal or not
+    def __eq__(self, other):
+        return self.word == other.word
+
     # be able to print out the card
+    def __str__(self):
+        return self.word
+
+
+if __name__ == '__main__':
+    card1 = Card('egg', 'A1')
+    card2 = Card('egg', 'B1')
+    card3 = Card('hut', 'D4')
+
+    print(card1 == card2)  # True
+    print(card1 == card3)  # False
+    print(card1)  # egg
